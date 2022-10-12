19b4015557ee97c79403be2cd969648301094115
dzp-datah <damita.f@ku.th>
2022-10-12 14:00:07 +0700

    update flake8

--- test_statistics.py
+++ test_statistics.py
@@ -6,9 +6,9 @@ class StatisticsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_throws_exception(self):
         """variance of an empty list should raise an exception"""
@@ -18,5 +18,5 @@ class StatisticsTest(TestCase):
 
 if __name__ == '__main__':
     import unittest
-    unittest.main(verbosity=1)
 
+    unittest.main(verbosity=1)
